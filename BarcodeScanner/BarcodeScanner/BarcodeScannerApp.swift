e12ab6e8d2df78f76d699b05008adfd089d47845
James <james.mcdougall@goldkey.com>
2021-07-26 20:48:48 -0500

    Initial Project Commit

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by James McDougall on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
